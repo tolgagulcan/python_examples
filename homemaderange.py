e55b9e12d9cac1ddc6f8a3f693f3f1a41c772e91
tolga gülcan <tolgagulcan@hotmail.com>
2018-05-20 18:18:31 +0300

    Initial commit

---
+++ homemaderange.py
@@ -0,0 +1,13 @@
+def myrange(start=None, end=None, step=1):
+    if not start:
+        start = 0
+        end = 0
+    if not end:
+        end = start
+        start = 0
+    num = start
+    while (num < end):
+        yield num
+        num = num + step*n
+t = list(myrange())
+print(t)
